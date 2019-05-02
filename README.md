2bcd4ca7184ea9ed9edbc25021c2e721a74c027f
Leigh Halliday <leighhalliday@gmail.com>
2019-05-02 11:28:52 -0400

    Google Maps in React demo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Google Maps in React
+
+YouTube video can be found here: [https://youtu.be/Pf7g32CwX_s](https://youtu.be/Pf7g32CwX_s)
+
+## Running Locally
+
+Google Maps API Key: Create a new API key from the Google developer console, making sure that `Maps JavaScript API` has been enabled. Copy that key into your `.env.local` file creating an ENV var with the name `REACT_APP_GOOGLE_KEY`.
---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import * as parkData from "./data/skateboard-parks.json";
+import mapStyles from "./mapStyles";
+
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {parkData.features.map(park => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0]
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+          icon={{
+            url: `/skateboarding.svg`,
+            scaledSize: new window.google.maps.Size(25, 25)
+          }}
+        />
+      ))}
+
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0]
+          }}
+        >
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_GOOGLE_KEY
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
